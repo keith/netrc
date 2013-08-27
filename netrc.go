fbd31ef4127e5846eab6b0cff7c92ba6c6ff906b
Keith Smiley <keithbsmiley@gmail.com>
2013-08-27 15:41:48 -0400

    :Fmt

--- netrc.go
+++ netrc.go
@@ -1,30 +1,30 @@
 package netrc
 
 import (
-    "errors"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "os/user"
-    "path/filepath"
-    "strings"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 )
 
 // A simple credential struct for a username and password
 type Cred struct {
-    User string
-    Pass string
+	User string
+	Pass string
 }
 
 // A struct for the Netrc
-//  Path is the netrc filepath
-//  Creds is a map of string keys that have pointers to Cred structs
-//  The keys relate to the service name from the netrc or 'default'
-//  ex: 'machine github.com' Then the key is github.com
+//	Path is the netrc filepath
+//	Creds is a map of string keys that have pointers to Cred structs
+//	The keys relate to the service name from the netrc or 'default'
+//	ex: 'machine github.com' Then the key is github.com
 type Netrc struct {
-    Path string
-    Creds map[string]*Cred
+	Path  string
+	Creds map[string]*Cred
 }
 
 // Opens the given path if it exists
@@ -32,155 +32,154 @@ type Netrc struct {
 // A new Netrc is created and the file is read into the Creds map
 // A pointer to the Netrc will always be returned
 func Open(path string) *Netrc {
-    filePath := FilePath(path)
-    if !FileExists(filePath) {
-        log.Fatalf("Netrc file does not exist in '%s'", filePath)
-    }
-    
-    n := new(Netrc)
-    n.Path = filePath
-    n.Read()
+	filePath := FilePath(path)
+	if !FileExists(filePath) {
+		log.Fatalf("Netrc file does not exist in '%s'", filePath)
+	}
 
-    return n
+	n := new(Netrc)
+	n.Path = filePath
+	n.Read()
+
+	return n
 }
 
 // Creates a new Cred struct with a user and pass
-//  u: a username
-//  p: a password
-//  if either u or p are the empty string an error is returned
-//  otherwise a pointer to the struct is returned
+//	u: a username
+//	p: a password
+//	if either u or p are the empty string an error is returned
+//	otherwise a pointer to the struct is returned
 func CreateCred(u string, p string) (*Cred, error) {
-    if u == "" || p == "" {
-        return nil, errors.New("Invalid arguments")
-    }
-    cred := new(Cred)
-    cred.User = u
-    cred.Pass = p
+	if u == "" || p == "" {
+		return nil, errors.New("Invalid arguments")
+	}
+	cred := new(Cred)
+	cred.User = u
+	cred.Pass = p
 
-    return cred, nil
+	return cred, nil
 }
 
 // Creates a file path based on the passed string
-//  if the empty string is passed $HOME/.netrc is used
-//  if a filename is passed ex: .mynetrc it is assumed to be in your
-//    $HOME directory, ex: $HOME/.mynetrc
-//  if a path starting with a / is passed it is used without modification
+//	if the empty string is passed $HOME/.netrc is used
+//	if a filename is passed ex: .mynetrc it is assumed to be in your
+//	  $HOME directory, ex: $HOME/.mynetrc
+//	if a path starting with a / is passed it is used without modification
 //
-//  this uses path/filepath's Join(...) function so you can also pass
-//    relative paths ex: "../.netrc" would return /Users/.netrc on OS X
+//	this uses path/filepath's Join(...) function so you can also pass
+//	  relative paths ex: "../.netrc" would return /Users/.netrc on OS X
 func FilePath(path string) string {
-    if strings.HasPrefix(path, "/") {
-        return path
-    }
+	if strings.HasPrefix(path, "/") {
+		return path
+	}
 
-    totalPath := ""
-    usr, err := user.Current()
-    if err != nil {
-        log.Fatal(err)
-    }
+	totalPath := ""
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    home := usr.HomeDir
-    file := ".netrc"
+	home := usr.HomeDir
+	file := ".netrc"
 
-    if (path != "") {
-        file = path
-    }
+	if path != "" {
+		file = path
+	}
 
-    totalPath = filepath.Join(home, file)
-    return totalPath
+	totalPath = filepath.Join(home, file)
+	return totalPath
 }
 
 // Check to see if the file exists at the given path
 func FileExists(path string) bool {
-    if _, err := os.Stat(path); os.IsNotExist(err) {
-        return false
-    }
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
 
-    return true
+	return true
 }
 
 // Check the file permissions at the given path
-//  600 is valid, all others are invalid
-//  returns false if there is an issue with the path
+//	600 is valid, all others are invalid
+//	returns false if there is an issue with the path
 func ValidPermissions(path string) bool {
-    info, err := os.Stat(path)
-    if err != nil {
-        return false
-    }
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
 
-    per := fmt.Sprintf("%o", os.FileMode.Perm(info.Mode()))
-    if per == "600" {
-        return true
-    }
+	per := fmt.Sprintf("%o", os.FileMode.Perm(info.Mode()))
+	if per == "600" {
+		return true
+	}
 
-    return false
+	return false
 }
 
 // Netrc instance methods
 
 // Read the netrc from the file
-//  This must be called from a valid netrc instance that has a Path
+//	This must be called from a valid netrc instance that has a Path
 func (n *Netrc) Read() {
-    path := n.Path
-    b, err := ioutil.ReadFile(path)
-    if err != nil {
-        log.Fatal(err)
-    }
+	path := n.Path
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    cs := make(map[string]*Cred)
+	cs := make(map[string]*Cred)
 
-    words := strings.Fields(string(b))
-    for i := 0; i < len(words); i++ {
-        word := strings.TrimSpace(words[i])
-        key, user, pass := "", "", ""
-        if word == "machine" {
-            key = strings.TrimSpace(words[i+1])
-            user = strings.TrimSpace(words[i+3])
-            pass = strings.TrimSpace(words[i+5])
-            i += 5
-        } else if word == "default" {
-            key = word
-            user = strings.TrimSpace(words[i+2])
-            pass = strings.TrimSpace(words[i+4])
-            i += 4
-        }
+	words := strings.Fields(string(b))
+	for i := 0; i < len(words); i++ {
+		word := strings.TrimSpace(words[i])
+		key, user, pass := "", "", ""
+		if word == "machine" {
+			key = strings.TrimSpace(words[i+1])
+			user = strings.TrimSpace(words[i+3])
+			pass = strings.TrimSpace(words[i+5])
+			i += 5
+		} else if word == "default" {
+			key = word
+			user = strings.TrimSpace(words[i+2])
+			pass = strings.TrimSpace(words[i+4])
+			i += 4
+		}
 
-        cred, err := CreateCred(user, pass)
-        if err != nil {
-            log.Fatal(err)
-        }
+		cred, err := CreateCred(user, pass)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        cs[key] = cred
-    }
+		cs[key] = cred
+	}
 
-    n.Creds = cs
+	n.Creds = cs
 }
 
 // Get a username and password for a service
-//  This must be called from a valid netrc instace that has been Read
+//	This must be called from a valid netrc instace that has been Read
 // Params
-//  key: the string following machine in the netrc that identifies the service
-//  def: pass true if you want the default credentials (if they exist)
-//       if there aren't any credentials for the given key
+//	key: the string following machine in the netrc that identifies the service
+//	def: pass true if you want the default credentials (if they exist)
+//		 if there aren't any credentials for the given key
 // Return
-//  string: the username
-//  string: the password
-//  error:  an error if one occurs
-//           either because of a no credentials or an invalid key
+//	string: the username
+//	string: the password
+//	error:	an error if one occurs
+//			 either because of a no credentials or an invalid key
 func (n *Netrc) GetValue(key string, def bool) (string, string, error) {
-    if n.Creds == nil {
-        return "", "", fmt.Errorf("No credentails loaded from %s", n.Path)
-    }
+	if n.Creds == nil {
+		return "", "", fmt.Errorf("No credentails loaded from %s", n.Path)
+	}
 
-    val := n.Creds[key]
-    if val == nil && def {
-        val = n.Creds["default"]
-    }
+	val := n.Creds[key]
+	if val == nil && def {
+		val = n.Creds["default"]
+	}
 
-    if val == nil {
-        return "", "", errors.New("No matching credentails")
-    } else {
-        return val.User, val.Pass, nil
-    }
+	if val == nil {
+		return "", "", errors.New("No matching credentails")
+	} else {
+		return val.User, val.Pass, nil
+	}
 }
-
--- netrc_test.go
+++ netrc_test.go
@@ -1,134 +1,133 @@
 package netrc
 
 import (
-    "testing"
-    "os"
-    "fmt"
-    "path"
+	"fmt"
+	"os"
+	"path"
+	"testing"
 )
 
 func TestFilePath(t *testing.T) {
-    tests := []struct {
-        in string
-        out string
-    }{
-        {"", "/Users/ksmiley/.netrc"},
-        {".netlc", "/Users/ksmiley/.netlc"},
-        {"/.netrc", "/.netrc"},
-    }
-    for _, test := range tests {
-        res := FilePath(test.in)
-        if res != test.out {
-            t.Errorf("Response for (%s) got (%s) expected (%s)", test.in, res, test.out)
-        }
-    }
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"", "/Users/ksmiley/.netrc"},
+		{".netlc", "/Users/ksmiley/.netlc"},
+		{"/.netrc", "/.netrc"},
+	}
+	for _, test := range tests {
+		res := FilePath(test.in)
+		if res != test.out {
+			t.Errorf("Response for (%s) got (%s) expected (%s)", test.in, res, test.out)
+		}
+	}
 }
 
 func TestPermissions(t *testing.T) {
-    tests := []struct {
-        name string
-        per  uint32
-    }{
-        {"foo", 0777},
-        {"bar", 0600},
-        {"baz", 0606},
-    }
-    for _, test := range tests {
-        file, _ := os.Create(test.name)
-        file.Chmod(os.FileMode(test.per))
+	tests := []struct {
+		name string
+		per  uint32
+	}{
+		{"foo", 0777},
+		{"bar", 0600},
+		{"baz", 0606},
+	}
+	for _, test := range tests {
+		file, _ := os.Create(test.name)
+		file.Chmod(os.FileMode(test.per))
 
-        res := ValidPermissions(test.name)
-        if fmt.Sprintf("%o", test.per) == "600" {
-            if !res {
-                t.Errorf("Got invalid permissions for name (%s) per (%o)", test.name, test.per)
-            }
-        } else if res {
-            t.Errorf("Expected error for name (%s)", test.name)
-        }
+		res := ValidPermissions(test.name)
+		if fmt.Sprintf("%o", test.per) == "600" {
+			if !res {
+				t.Errorf("Got invalid permissions for name (%s) per (%o)", test.name, test.per)
+			}
+		} else if res {
+			t.Errorf("Expected error for name (%s)", test.name)
+		}
 
-        os.Remove(test.name)
-    }
+		os.Remove(test.name)
+	}
 }
 
 func TestRead(t *testing.T) {
-    tests := [][]byte {
-        []byte("machine foobar username bazqix password sekret"),
-    }
-    
-    for _, test := range tests {
-        wd, _ := os.Getwd()
-        file, _ := os.Create("foo")
-        file.Write(test)
-        filepath := path.Join(wd, "foo")
-        n := Open(filepath)
+	tests := [][]byte{
+		[]byte("machine foobar username bazqix password sekret"),
+	}
 
-        if n.Path != filepath {
-            t.Errorf("Expected path to be (%s) got (%s)", filepath, n.Path)
-        }
+	for _, test := range tests {
+		wd, _ := os.Getwd()
+		file, _ := os.Create("foo")
+		file.Write(test)
+		filepath := path.Join(wd, "foo")
+		n := Open(filepath)
 
-        os.Remove(filepath)
-    }
+		if n.Path != filepath {
+			t.Errorf("Expected path to be (%s) got (%s)", filepath, n.Path)
+		}
+
+		os.Remove(filepath)
+	}
 }
 
 func TestGetValue(te *testing.T) {
-    tests := []struct {
-        key string
-        u string
-        p string
-        def bool
-    }{
-        {"qux", "du", "dp", true}, // Default usage
-        {"foo", "foou", "foop", false}, // Specific usage
-        {"bar", "baru", "barp", true}, // Specific default fallback
-        {"baz", "", "", false}, // Specific no key
-    }
+	tests := []struct {
+		key string
+		u   string
+		p   string
+		def bool
+	}{
+		{"qux", "du", "dp", true},      // Default usage
+		{"foo", "foou", "foop", false}, // Specific usage
+		{"bar", "baru", "barp", true},  // Specific default fallback
+		{"baz", "", "", false},         // Specific no key
+	}
 
-    wd, _ := os.Getwd()
-    file, _ := os.Create("foo")
-    content := []byte("machine foo username foou password foop default username du password dp machine bar username baru password barp")
-    file.Write(content)
-    filepath := path.Join(wd, "foo")
-    n := Open(filepath)
+	wd, _ := os.Getwd()
+	file, _ := os.Create("foo")
+	content := []byte("machine foo username foou password foop default username du password dp machine bar username baru password barp")
+	file.Write(content)
+	filepath := path.Join(wd, "foo")
+	n := Open(filepath)
 
-    for _, t := range tests {
-        ru, rp, _ := n.GetValue(t.key, t.def)
-        if ru != t.u || rp != t.p {
-            te.Errorf("Expected u: (%s) p: (%s) got u: (%s) p: (%s)", t.u, t.p, ru, rp)
-        }
-    }
+	for _, t := range tests {
+		ru, rp, _ := n.GetValue(t.key, t.def)
+		if ru != t.u || rp != t.p {
+			te.Errorf("Expected u: (%s) p: (%s) got u: (%s) p: (%s)", t.u, t.p, ru, rp)
+		}
+	}
 
-    os.Remove(filepath)
+	os.Remove(filepath)
 }
 
 func TestCreateCred(t *testing.T) {
-    tests := []struct {
-        u string
-        p string
-        pass bool
-    }{
-        {"foo", "bar", true},
-        {"", "baz", false},
-        {"qux", "", false},
-    }
-    for _, test := range tests {
-        res, err := CreateCred(test.u, test.p)
-        if test.pass {
-            if err != nil {
-                t.Errorf("Got error (%s) for user: (%s) pass: (%s)", err, test.u, test.p)
-            }
+	tests := []struct {
+		u    string
+		p    string
+		pass bool
+	}{
+		{"foo", "bar", true},
+		{"", "baz", false},
+		{"qux", "", false},
+	}
+	for _, test := range tests {
+		res, err := CreateCred(test.u, test.p)
+		if test.pass {
+			if err != nil {
+				t.Errorf("Got error (%s) for user: (%s) pass: (%s)", err, test.u, test.p)
+			}
 
-            if res.User != test.u || res.Pass != test.p {
-                t.Errorf("Expected u: (%s) p: (%s) got u: (%s) p: (%s)", test.u, test.p, res.User, res.Pass)
-            }
-        } else {
-            if err == nil {
-                t.Errorf("Expected error u: (%s) p: (%s)", test.u, test.p)
-            }
+			if res.User != test.u || res.Pass != test.p {
+				t.Errorf("Expected u: (%s) p: (%s) got u: (%s) p: (%s)", test.u, test.p, res.User, res.Pass)
+			}
+		} else {
+			if err == nil {
+				t.Errorf("Expected error u: (%s) p: (%s)", test.u, test.p)
+			}
 
-            if res != nil {
-                t.Errorf("Expected nil res got: (%+v)", res)
-            }
-        }
-    }
+			if res != nil {
+				t.Errorf("Expected nil res got: (%+v)", res)
+			}
+		}
+	}
 }
-
